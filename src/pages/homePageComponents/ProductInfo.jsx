e5e90db74c67ba132f9819e2849875ef0a89f7ef
dammamonnankulama <dammamonnankulama@gmail.com>
2025-01-11 00:19:20 +0530

    Add ImageSlider component to ProductInfo; enhance product display with image carousel😎😎

--- src/pages/homePageComponents/ProductInfo.jsx
+++ src/pages/homePageComponents/ProductInfo.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ProductNotFound from "./ProductNotFound";
+import ImageSlider from "../../components/ImageSlider";
 
 function ProductInfo() {
   // useParams is a hook that allows you to access the URL parameters in your component.
@@ -50,8 +51,23 @@ function ProductInfo() {
         )
       }
         {
-            status== "found" && <h1>Product found</h1>
-        }
+        status == "found"&&(
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="w-[35%] h-full">
+            <ImageSlider images={product.productImages}/>
+          </div>
+          <div className="w-[65%] h-full p-4">
+            <h1 className="text-3xl font-bold text-gray-800">{product.productName}</h1>
+            <h1 className="text-3xl font-bold text-gray-500">{product.altNames.join(" | ")}</h1>
+            <p className="text-xl text-gray-600">{
+            (product.price>product.lastPrice)&&
+            <span className="line-through text-red-500">LKR.{product.price}</span>
+            } <span>{"LKR."+product.lastPrice}</span></p>
+            <p className="text-lg text-gray-600 line-clamp-3">{product.description}</p>
+          </div>
+        </div>
+        )
+      }
 
 
     </div>
